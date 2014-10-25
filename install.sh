2abf298d9496dfee8c3c2843be431c920aec7f75
Maxim N. Kostrikin <mkostrikin@gmail.com>
2014-10-26 01:09:29 +0700

    Added Docker setup

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ apt-get update
 apt-get install curl
 
 curl -sSL  https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
 echo deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main 9.4 | sudo tee /etc/apt/sources.list.d/postgres.list
 
 #percona mysql
@@ -15,10 +16,17 @@ echo deb http://nginx.org/packages/ubuntu/ trusty nginx | sudo tee /etc/apt/sour
 
 wget -qP /tmp/ http://repo.zabbix.com/zabbix/2.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.4-1+trusty_all.deb && dpkg -i /tmp/zabbix-release_2.4-1+trusty_all.deb && rm -rf /tmp/zabbix-release_2.4-1+trusty_all.deb
 
-apt-get update
+sed -i "/^# deb .*partner/ s/^# //; /multiverse/d" /etc/apt/sources.list
+echo "deb http://ru.archive.ubuntu.com/ubuntu/ trusty multiverse" >> /etc/apt/sources.list
+echo "deb http://ru.archive.ubuntu.com/ubuntu/ trusty-updates multiverse" >> /etc/apt/sources.list
+
+curl -sSL https://get.docker.io/ubuntu/ | sh
+
 apt-get install percona-server-server-5.6 percona-server-client-5.6 \
 postgresql-9.4 \
 nginx \
-zabbix-agent
+zabbix-agent \
+ssh vim mc iptraf bash-completion tcpdump mtr-tiny screen tmux rar unrar zip unzip lsof whois rlwrap dstat sudo xz-utils git mercurial etckeeper bridge-utils haveged strace ntp xfsprogs
+
 
 
